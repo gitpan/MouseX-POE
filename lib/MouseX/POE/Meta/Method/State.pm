03e44e48bb414981c034b2c1b561cb70d0547aff
Chris Williams <chris@bingosnet.co.uk>
2011-05-13 18:41:13 +0000

    Import of BINGOS/MouseX-POE-0.213 from CPAN.
    gitpan-cpan-distribution: MouseX-POE
    gitpan-cpan-version:      0.213
    gitpan-cpan-path:         BINGOS/MouseX-POE-0.213.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/MouseX/POE/Meta/Method/State.pm
@@ -0,0 +1,61 @@
+package MouseX::POE::Meta::Method::State;
+BEGIN {
+  $MouseX::POE::Meta::Method::State::VERSION = '0.213';
+}
+# ABSTRACT: Dont look into this, its boring
+use strict;
+use Mouse;
+extends qw(Mouse::Meta::Method);
+
+no Mouse;
+1;
+
+
+=pod
+
+=head1 NAME
+
+MouseX::POE::Meta::Method::State - Dont look into this, its boring
+
+=head1 VERSION
+
+version 0.213
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Chris Prather <chris@prather.org>
+
+=item *
+
+Ash Berlin <ash@cpan.org>
+
+=item *
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=item *
+
+Yuval (nothingmuch) Kogman
+
+=item *
+
+Torsten Raudssus <torsten@raudssus.de> L<http://www.raudssus.de/>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Chris Prather, Ash Berlin, Chris Williams, Yuval Kogman, Torsten Raudssus.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
